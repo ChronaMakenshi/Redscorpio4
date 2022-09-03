3e7577da4e4288d1fb78a8c9cd9ae98d9d92f39e
Mestdagh christophe <77621850+ChronaMakenshi@users.noreply.github.com>
2022-09-03 10:39:53 +0200

    email

--- src/Controller/Redscorpio4Controller.php
+++ src/Controller/Redscorpio4Controller.php
@@ -4,13 +4,17 @@ namespace App\Controller;
 
 use App\Entity\BoiteEmail;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\MailerInterface;
+use Karser\Recaptcha3Bundle\Form\Recaptcha3Type;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\HttpFoundation\Request;
+use Karser\Recaptcha3Bundle\Validator\Constraints\Recaptcha3;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class RedScorpio4Controller extends AbstractController
 {
@@ -18,7 +22,7 @@ class RedScorpio4Controller extends AbstractController
      * @Route("/", name="redscorpio4")
      */
 
-    public function index(BoiteEmail $contact = null,Request $request,EntityManagerInterface $manager): Response
+    public function sendEmail(BoiteEmail $contact = null,Request $request,EntityManagerInterface $manager, MailerInterface $mailer): Response
     {
         
         $contact = new BoiteEmail;
@@ -27,7 +31,11 @@ class RedScorpio4Controller extends AbstractController
         $form = $this->createFormBuilder($contact)
                     ->add('Name', TextType::class, array('label' => false))
                     ->add('Mail', EmailType::class, array('label' => false)) 
-                    ->add('Message', TextareaType::class, array('label' => false))  
+                    ->add('Message', TextareaType::class, array('label' => false))
+                    ->add('captcha', Recaptcha3Type::class, [
+                      'constraints' => new Recaptcha3(),
+                      'action_name' => 'contact',
+                    ])
                     ->getForm();
                        
           $form->handleRequest($request);
@@ -35,6 +43,21 @@ class RedScorpio4Controller extends AbstractController
             $this->addFlash('success', 'Your message has been sent !');
             $manager->persist($contact);
             $manager->flush();
+
+            $email = (new TemplatedEmail())
+            ->from($contact->getMail())
+            ->to('alois.patoor@redscorpio4.com')    
+            ->subject($contact->getName())    
+            // path of the Twig template to render
+            ->htmlTemplate('emails/contact.html.twig')
+
+            // pass variables (name => value) to the template
+            ->context([
+                'contact' => $contact,
+                'username' => 'alois.patoor',
+            ]);
+           
+            $mailer->send($email);
   
             return $this->redirectToRoute('redscorpio4');
           }
@@ -43,4 +66,5 @@ class RedScorpio4Controller extends AbstractController
             'controller_name' => 'Redscorpio4Controller',
         ]);
     }
+    
 }
